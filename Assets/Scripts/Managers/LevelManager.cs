b1b36ea1132fbf4b2888ebb8552950c364c2544e
Ethan <72696784+Boolwark@users.noreply.github.com>
2023-11-08 08:36:21 +0800

    add loading bar
    added a loading bar to the first level. Scene Loading is now async

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,20 +1,41 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Util
 {
     public class LevelManager  : Singleton<LevelManager>
     {
+        public GameObject loadingScreen;
+        public Slider slider;
+        public TextMeshProUGUI progressText;
         public UnityEvent onSceneChanged;
         public int currentSceneIndex = -1; // Menu scene starts first, is scene index -1.
         public void ChangeScene(string sceneName)
         {
             currentSceneIndex++;
-            SceneManager.LoadScene(sceneName);
+            StartCoroutine(LoadLevelAsync(sceneName));
             onSceneChanged?.Invoke();
+        }
+
+        IEnumerator LoadLevelAsync(string sceneName)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+            loadingScreen.SetActive(true);
+            while (!operation.isDone)
+            {
+                float progress = Mathf.Clamp01(operation.progress / 0.9f);
+                Debug.Log(progress);
+                slider.value = progress;
+                progressText.text = progress * 100f + "%";
+                yield return null;
+            }
         }
 
      
