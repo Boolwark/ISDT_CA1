8a8965fbe2f04cabc6e410a0c0cf4977fbf3974c
Ethan <72696784+Boolwark@users.noreply.github.com>
2023-12-11 23:12:51 +0800

    added spawn loot crate effect
    when player activates the beacon a loot crate spawns
    why am i doing this

--- Assets/Scripts/Environment/Battery/Battery.cs
+++ Assets/Scripts/Environment/Battery/Battery.cs
@@ -1,3 +1,4 @@
+using CodeMonkey.Utils;
 using UnityEngine;
 
 namespace Environment.Battery
@@ -5,13 +6,37 @@ namespace Environment.Battery
     public class Battery : MonoBehaviour
     {
         private bool activated = false;
+        public float dropHeight = 5f;
+        public float effectDuration;
+        public GameObject lootCratePf;
+        public GameObject activateEffect;
         public void OnSelect()
         {
             if (!activated)
             {
                 BatteryManager.Instance.OnBatteryActivated();
+                //Spawn Loot drop
+                SpawnLootDrop();
+                PlayActivateEffect();
             }
             activated = true;
+        }
+
+        private void SpawnLootDrop()
+        {
+            var lootCrate = Instantiate(lootCratePf, transform.position + new Vector3(0f, dropHeight, 0f), Quaternion.identity);
+            var rb = lootCrate.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            FunctionTimer.Create(() => { rb.isKinematic = true; },2f);
+        }
+
+        private void PlayActivateEffect()
+        {
+            activateEffect.SetActive(true);
+            FunctionTimer.Create(() =>
+            { activateEffect.SetActive(false);
+            },effectDuration);
         }
     }
 }
